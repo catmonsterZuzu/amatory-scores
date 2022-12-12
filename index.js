43e8e482ee834f3e1584f20fd9b974382d8cff52
CatmonsterZuzu <jacob.porter.dev@gmail.com>
2022-12-11 18:33:26 -0700

    Getting dependencies set up for upcoming project.

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+// $(function () {
+
+//     $('#test-button').on('click', function() {
+//         console.log('something');
+//     });
+
+
+//     const DATA_COUNT = 7;
+//     const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 100};
+
+//     const labels = Utils.months({count: 7});
+//     const data = {
+//     labels: labels,
+//     datasets: [
+//         {
+//         label: 'Dataset 1',
+//         data: Utils.numbers(NUMBER_CFG),
+//         borderColor: Utils.CHART_COLORS.red,
+//         backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+//         },
+//         {
+//         label: 'Dataset 2',
+//         data: Utils.numbers(NUMBER_CFG),
+//         borderColor: Utils.CHART_COLORS.blue,
+//         backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+//         }
+//     ]
+//     };
+
+//     const config = {
+//         type: 'radar',
+//         data: data,
+//         options: {
+//           responsive: true,
+//           plugins: {
+//             title: {
+//               display: true,
+//               text: 'Chart.js Radar Chart'
+//             }
+//           }
+//         },
+//       };
+
+
+// });
